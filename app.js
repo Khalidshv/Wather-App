40ac587e0ab566838df6f7d9bd528c539b434ba6
X <x@Xs-MacBook-Pro.local>
2024-01-31 00:09:25 +0400

    js section

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const apiKey = "";
 const apiUrl = "";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + '&appid-${apiKey}');
+const searchBox = document. querySelector(" search input");
+const searchBt = document. querySelector(" search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +  '&appid-${apiKey}');
     var data = await response.json();
+
     console.log(data);
 
     document.querySelector("city").innerHTML = data.name;
+    document.querySelector("temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector("humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector("wind").innerHTML = data.wind.speed + "km/h";
+
+    searchBtn.addEventListener("click", ()=>{
+        checkWeather (searchBox.value);
+    })
 }
 
 checkWeather();
